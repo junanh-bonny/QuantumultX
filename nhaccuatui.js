7fa73dba7c30127ed0265cdb2c0ccbd44c4a8c3c
JunAnh <69787284+junanh-bonny@users.noreply.github.com>
2020-09-17 14:59:22 +0800

    Update nhaccuatui.js

--- nhaccuatui.js
+++ nhaccuatui.js
@@ -1,11 +1,10 @@
-# Copyright：Langkhach270389
-
+  
 var obj = JSON.parse($response.body);
 let url = $request.url;
 var cons = "users/info";
 if(url.indexOf(cons) != -1)
 {
-obj.data.VIPExpire= "06/09/2999 00:00:00";
+obj.data.VIPExpire= "27/03/9999 00:00:00";
 obj.data.isVIP= true;
 }
 $done({body: JSON.stringify(obj)});
