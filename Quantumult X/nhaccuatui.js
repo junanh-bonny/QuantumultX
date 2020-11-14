5974bb792c72e234742ef03b086f4ff56cdf6c98
JunAnh <69787284+junanh-bonny@users.noreply.github.com>
2020-11-14 09:55:55 +0800

    Update nhaccuatui.js

--- Quantumult X/nhaccuatui.js
+++ Quantumult X/nhaccuatui.js
@@ -3,7 +3,7 @@ let url = $request.url;
 var cons = "users/info";
 if(url.indexOf(cons) != -1)
 {
-obj.data.VIPExpire= "06/09/2099 00:00:00";
+obj.data.VIPExpire= "07/09/2099 00:00:00";
 obj.data.isVIP= true;
 }
 $done({body: JSON.stringify(obj)});
